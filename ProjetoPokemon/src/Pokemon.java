f966ed841e8b95de79aee733ac072b65cdf4b3a2
wesley <wesley@wesley-X510UAR>
2019-05-01 16:21:35 -0300

    Testando

---
+++ ProjetoPokemon/src/Pokemon.java
@@ -0,0 +1,31 @@
+
+/*
+ *  */
+
+public class Pokemon {
+	private int MaxHP;
+	private int tipoDoPokemon;
+	private String nomePokemon;
+	private Habilidades Hab;
+	private int HP;
+	
+	public Pokemons(int HP, int tipoDoPokemon, String nomePokemon, String[] hab, int[] powerHab) {
+		this.HP = HP;
+		this.tipoDoPokemon = tipoDoPokemon;
+		this.nomePokemon = nomePokemon;
+		hab = new Habilidades (hab, powerHab);
+	}
+	public int getHP() {
+		return HP;
+	}
+	public void setHP(int HP) {
+		this.HP = HP;
+	}
+	public int gettipoDoPokemon() {
+		return tipoDoPokemon;
+	}
+	public String getnomeDoPokemon() {
+		return nomePokemon;
+	}
+	
+}
