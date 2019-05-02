f99e3d2bc03336a9c83ac9f84b31187bbeb9c615
Artur <arturvribeiro@usp.br>
2019-05-02 09:56:17 -0300

    Versão 5

--- src/Treinador.java
+++ src/Treinador.java
@@ -2,22 +2,43 @@ public class Treinador {
 	private String nome;
 	private Pokemon[] deck = new Pokemon[6];
 	private int pokemonAtivo;
-	private int poções;
+	private item[] poções = new item[4];
 	private static int qtPokemons = 0;
 	
-	public Treinador(String nome, int poções) {
+	public Treinador(String nome) {
 		this.nome = nome;
-		this.poções = poções;
 	}
 	
-	public void adicionaPokemon(int HP, String tipo, String[] habilidades, int[] potencia) {
+	public void adicionaPokemon(int HP, String tipo, String nome, String[] habilidades, int[] potencia) {
 		if (qtPokemons < 6) {
-			deck[qtPokemons] = new Pokemon(HP, tipo, habilidades, potencia);
+			deck[qtPokemons] = new Pokemon(HP, tipo, nome, habilidades, potencia);
 			qtPokemons++;
 		}
 	}
-	
+	public void adicionaPoção(String nome, int pontos) {
+		int i = 0;
+		while (i < 3) {
+			if (poções[i] == null) {
+				poções[i] = new item(nome, pontos);
+				i = 4;
+			}else i++;
+		}
+		if (i == 3) System.out.println("Não pode adicionar poção, limite máximo de 3");
+	}
+	public void removePoção(int x) {
+		poções[x] = null;
+	}
+	public void removePokemon(int numeroDoPokemon) {
+		deck[numeroDoPokemon] = null;
+	}
 	public void atacarPokemon (int idx, Pokemon inimigo) {
-		
+		inimigo.perdeHP(deck[pokemonAtivo].getPotenciaHabilidade(idx));
+	}
+	public void usarItem(int x) {
+		deck[pokemonAtivo].curaHP(poções[x].getPontos());
+		removePoção(x);
+	}
+	public boolean fugir() {
+		return true;
 	}
 }
